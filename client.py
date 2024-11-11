49d9e1c773f82054f9192510d5eaa1310fffea5e
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 13:11:06 +0300

    shut down client

--- client.py
+++ client.py
@@ -10,27 +10,41 @@ def receive_messages(client_socket):
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
-            print(message)
+            if message:
+                print(message)
+            else:
+                print("Server closed the connection.")
+                client_socket.close()
+                break
         except:
-            print("Connection closed by server.")
+            print("Disconnected from server.")
             client_socket.close()
             break
 
 # Main client function
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((SERVER_HOST, SERVER_PORT))
+    try:
+        client_socket.connect((SERVER_HOST, SERVER_PORT))
+    except ConnectionRefusedError:
+        print("Server is unavailable. Exiting.")
+        return
     
     # Start receiving thread
     thread = threading.Thread(target=receive_messages, args=(client_socket,))
     thread.start()
 
-    while True:
-        message = input()
-        if message.lower() == 'exit':
-            client_socket.close()
-            break
-        client_socket.send(message.encode('utf-8'))
+    try:
+        while True:
+            message = input()
+            if message.lower() == 'exit':
+                client_socket.close()
+                print("You have exited the chat.")
+                break
+            client_socket.send(message.encode('utf-8'))
+    except KeyboardInterrupt:
+        print("\nExiting chat...")
+        client_socket.close()
 
 if __name__ == "__main__":
     start_client()

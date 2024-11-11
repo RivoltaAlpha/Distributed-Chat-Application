6328ca3017bf003344f33530a4e2868488b07cf5
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-11 12:27:40 +0300

    initial code

---
+++ server.py
@@ -0,0 +1,51 @@
+import socket
+import threading
+
+# Server configuration
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 12345
+clients = []
+
+# Broadcast message to all connected clients
+def broadcast(message, client_socket):
+    for client in clients:
+        if client != client_socket:
+            try:
+                client.send(message)
+            except:
+                # Remove clients that are disconnected
+                clients.remove(client)
+
+# Handle individual client connections
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if message:
+                print(f"Received: {message.decode('utf-8')}")
+                broadcast(message, client_socket)
+            else:
+                client_socket.close()
+                clients.remove(client_socket)
+                break
+        except:
+            client_socket.close()
+            clients.remove(client_socket)
+            break
+
+# Main server function
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
+    server_socket.listen()
+    print("Server listening on", SERVER_HOST, ":", SERVER_PORT)
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"New connection from {client_address}")
+        clients.append(client_socket)
+        thread = threading.Thread(target=handle_client, args=(client_socket,))
+        thread.start()
+
+if __name__ == "__main__":
+    start_server()
